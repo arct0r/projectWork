0bf658c569e19cae09728e759b283d8739eeb9d6
arct0r <109032203+arct0r@users.noreply.github.com>
2024-06-11 11:51:29 +0200

    Now we can load the page.html

--- scraping.py
+++ scraping.py
@@ -2,7 +2,9 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
 
-page = open("quacker.html", "r")
-soup1 = BeautifulSoup(page, 'html.parser')
+with open('quacker.html', errors='ignore') as page:
+    soup = BeautifulSoup(page, 'html.parser')
 
-print(page.prettify())
+    
+print(soup.prettify())
+
