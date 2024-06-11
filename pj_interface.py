10c284835e5b1dc83c5a793f3ba425b12231f0aa
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-06-11 14:56:13 +0200

    Update pj_interface.py

--- pj_interface.py
+++ pj_interface.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests as rq
+from scraping import getDF
 
 st.title('Substance Searcher (sample name)')
 
@@ -11,18 +12,17 @@ with open("cir-reports.csv") as tab:
 csv = csv[2:]
 csv = csv.drop_duplicates(subset=['Link'])
 
-substance = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=csv['Ingredienti'])
+substance = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=csv['Ingredienti'], index=None)
 #st.write("Attempting to search: ", substance)
+if substance:
+    result = csv.loc[csv['Ingredienti'] == substance]
+    link = result.iloc[0]['Link']   
+    link
+    page_ref = rq.get(link)
+    page_soup = bs(page_ref.text, 'html.parser')
+else:
+    csv
 
-result = csv.loc[csv['Ingredienti'] == substance]
-link = result.iloc[0]['Link']
-link
-page_ref = rq.get(link)
-
-page_soup = bs(page_ref.text, 'html.parser')
-# print(page_soup.prettify())
-
-#page_soup.findAll('table')[0].findAll('tr')
-['https://cir-reports.cir-safety.org/'+i.get('href')[3:] for i in page_soup.findAll('table')[0].findAll('a')]
+getDF(link)     
 
 
