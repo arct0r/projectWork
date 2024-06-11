c3a001cd57cb41d530e1208a095a5303ab4a4a44
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-06-11 14:38:04 +0200

    Added the getDF function

--- scraping.py
+++ scraping.py
@@ -1,12 +1,21 @@
 # Qui dal sito creiamo una tabella pandas
 import pandas as pd 
-from bs4 import BeautifulSoup
+import requests as rq
+from bs4 import BeautifulSoup as bs
 
 # with open('quacker.html', errors='ignore') as page:
     #soup = BeautifulSoup(page, 'html.parser')
 
-    
 #print(soup.prettify())
 
+# Funzione che crea il dataframe con tutte le report e le reference, una volta definito l'ingrediente,
+# prende in input il link della pagina contenente tutte le report dell'ingrediente
+def getDF(link):
+    page_ref = rq.get(link)
 
+    page_soup = bs(page_ref.text, 'html.parser')
+    # print(page_soup.prettify())
+
+    ['https://cir-reports.cir-safety.org/'+i.get('href')[3:] for i in page_soup.findAll('table')[0].findAll('a')]   
+    return 0
 
