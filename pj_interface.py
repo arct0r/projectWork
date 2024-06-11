63006e4041ab6adeff0219cd0f4e9c827f44267b
arct0r <109032203+arct0r@users.noreply.github.com>
2024-06-11 12:22:46 +0200

    Implemented Substance Selection

--- pj_interface.py
+++ pj_interface.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Substance Searcher (sample name)')
 
-substance = st.text_input("Inserisci il nome della sostanza che vuoi cercare:")
-st.write("Attempting to search: ", substance)
+with open("cir-reports.csv") as tab:
+    csv = pd.read_csv(tab, names=['Ingredienti', 'INCI Nome', 'Link'])
 
+csv = csv[2:]
+substance = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=csv['Ingredienti'])
+#st.write("Attempting to search: ", substance)
+
+csv.loc[csv['Ingredienti'] == substance]
--- scraping.py
+++ scraping.py
@@ -2,9 +2,11 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
 
-with open('quacker.html', errors='ignore') as page:
-    soup = BeautifulSoup(page, 'html.parser')
+# with open('quacker.html', errors='ignore') as page:
+    #soup = BeautifulSoup(page, 'html.parser')
 
     
-print(soup.prettify())
+#print(soup.prettify())
+
+
 
