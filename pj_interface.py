56d747a7af02a8eeb099ed527954ba792c3fef43
arct0r <109032203+arct0r@users.noreply.github.com>
2024-06-12 12:36:04 +0200

    Added a button to select the source

--- pj_interface.py
+++ pj_interface.py
@@ -4,27 +4,35 @@ from bs4 import BeautifulSoup as bs
 import requests as rq
 from scraping import getDF
 
-st.title('⌬ Substance Searcher  ')
+st.title('⌬ Substance Searcher 🧪')
 
-with open("cir-reports.csv") as tab:
-    csv = pd.read_csv(tab, names=['Ingredienti', 'INCI Nome', 'Link'])
+#Bottone
+source = st.radio(
+    "Seleziona la fonte di dati",
+    [":rainbow[ECHA]", ":blue[CIR]"],
+    captions = ["Più veloce, attraverso l'API di Iuclid", "Scraping manuale, lento"], horizontal=True)
 
-csv = csv[2:]
-csv = csv.drop_duplicates(subset=['Link'])
+if source == ":rainbow[ECHA]":
+    "Hey, you shouldn't click that yet."
+elif source ==':blue[CIR]':
+    with open("cir-reports.csv") as tab:
+        csv = pd.read_csv(tab, names=['Ingredienti', 'INCI Nome', 'Link'])
 
-substance = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=csv['Ingredienti'], index=None)
-#st.write("Attempting to search: ", substance)
-if substance:
-    result = csv.loc[csv['Ingredienti'] == substance]
-    link = result.iloc[0]['Link']   
-    link
-    page_ref = rq.get(link)
-    page_soup = bs(page_ref.text, 'html.parser')
-    table = getDF(link)
-    table     
-else:
-    csv
+    csv = csv[2:]
+    csv = csv.drop_duplicates(subset=['Link'])
 
+    substance = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=csv['Ingredienti'], index=None)
+    #st.write("Attempting to search: ", substance)
+    if substance:
+        result = csv.loc[csv['Ingredienti'] == substance]
+        link = result.iloc[0]['Link']   
+        link
+        page_ref = rq.get(link)
+        page_soup = bs(page_ref.text, 'html.parser')
+        table = getDF(link)
+        table     
+    else:
+        csv
 
 
 
