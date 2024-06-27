26c6216f7097aa9dc006a130a2ef59198447915f
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-06-27 09:46:55 +0200

    Update pj_interface.py

--- pj_interface.py
+++ pj_interface.py
@@ -38,7 +38,7 @@ elif source ==':blue[CIR]':
         genai.configure(api_key = "AIzaSyDBaM35Zp4FUO0ZDe01OsBpqsTUColrYyw")
         model = genai.GenerativeModel(model_name="gemini-1.5-flash")
         # domanda = st.text_area(label='Type your input here')
-        testo = pd.open('pdf.txt')
+        testo = pd.open_csv('pdf.txt', sep=" ", header=None)
         qq = f'Leggi questo testo e trova i valori di NOAEL e di LD50s presenti, poi creami una tabella che posso trasformare in un dataframe di pandas per Python\n{testo}'
         confirm = st.button(label='Ask gemini')
         if confirm:
