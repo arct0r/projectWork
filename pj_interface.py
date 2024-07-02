7d1801d5ab3c0befeed1f4ccccf9b687e5f3917d
arct0r <109032203+arct0r@users.noreply.github.com>
2024-07-02 17:50:39 +0200

    testing selenium on cloud

--- pj_interface.py
+++ pj_interface.py
@@ -4,9 +4,40 @@ from bs4 import BeautifulSoup as bs
 import requests as rq
 from scraping import getDF, get_pdf_pages
 import google.generativeai as genai
+import time 
 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+from bs4 import BeautifulSoup
+
+def get_website_content(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+        time.sleep(5)
+        html_doc = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup.get_text()
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
 
 st.title('⌬ Substance Searcher 🧪')
+
+
 
 #Bottone
 source = st.radio(
@@ -15,7 +46,19 @@ source = st.radio(
     captions = ["Più veloce, attraverso l'API di Iuclid", "Scraping manuale, lento"], horizontal=True, index=1)
 
 if source == ":rainbow[ECHA]":
-    "Hey, you shouldn't click that yet."
+    echastuff = pd.read_excel('echastuff.xlsx')
+    
+    echa_substance_select = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=echastuff['Substance'], index=None)
+    if not echa_substance_select:
+        echastuff
+
+    # Se schiaccio una sostanza echa dal multiselect
+    if echa_substance_select:      
+        with st.status("Downloading data...", expanded=True) as status:
+            st.write("Cerco la sostanza selezionata sul sito dell'echa")
+            content = get_website_content('https://chem.echa.europa.eu/')
+            st.write(content)
+
 elif source ==':blue[CIR]':
     with open("cir-reports.csv") as tab:
         csv = pd.read_csv(tab, names=['Ingredienti', 'INCI Nome', 'Link'])
