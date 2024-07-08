fbd8da89ba6840812c07e4d9954f184c36ad45ef
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-07-08 15:01:32 +0200

    Update pj_interface.py

--- pj_interface.py
+++ pj_interface.py
@@ -115,7 +115,6 @@ elif source ==':blue[CIR]':
         table = getDF(link)
         dossier_link = table.iloc[0]['Link']
         dossier_text = get_pdf_content(dossier_link)
-        dossier_text
         genai.configure(api_key = "AIzaSyDBaM35Zp4FUO0ZDe01OsBpqsTUColrYyw")
         model = genai.GenerativeModel(model_name="gemini-1.5-flash")
         # domanda = st.text_area(label='Type your input here')
