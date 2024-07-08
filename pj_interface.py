73f45c89cfa8f7dbe5c3d0c518e39184feca0f55
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-07-08 15:06:54 +0200

    Update pj_interface.py

--- pj_interface.py
+++ pj_interface.py
@@ -114,6 +114,7 @@ elif source ==':blue[CIR]':
         link = result.iloc[0]['Link']  
         table = getDF(link)
         dossier_link = table.iloc[0]['Link']
+        dossier_link
         dossier_text = get_pdf_content(dossier_link)
         dossier_text
         genai.configure(api_key = "AIzaSyDBaM35Zp4FUO0ZDe01OsBpqsTUColrYyw")
