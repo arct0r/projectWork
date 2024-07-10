092a907cacb0044db92b5e6ae162ebece3cf973d
arct0r <109032203+arct0r@users.noreply.github.com>
2024-07-10 16:28:15 +0200

    testing merging of interface

--- pj_interface.py
+++ pj_interface.py
@@ -5,35 +5,10 @@ import requests as rq
 from scraping import getDF, get_pdf_content
 import google.generativeai as genai
 import time 
+from pages.echa_find import search_dossier
+from pages.echa_summary import echa_pandas
+import re as standardre
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-
-
-def get_website_content(url):
-    driver = None
-    try:
-        # Using on Local
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                  options=options)
-        st.write(f"DEBUG:DRIVER:{driver}")
-        driver.get(url)
-        time.sleep(5)
-        html_doc = driver.page_source
-        driver.quit()
-        soup = bs(html_doc, "html.parser")
-        return soup.get_text()
-    except Exception as e:
-        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
-    finally:
-        if driver is not None: driver.quit()
-    return None
 
 col1, col2 = st.columns([7,3])
 with col1:
@@ -52,50 +27,17 @@ if source == ":rainbow[ECHA]":
     echa_substance_select = st.selectbox(label = "Inserisci il nome della sostanza che vuoi cercare:", options=echastuff['Substance'], index=None)
     if not echa_substance_select:
         echastuff
-
-
-
-
     # Se schiaccio una sostanza echa dal multiselect
     if echa_substance_select:      
-        with st.status("Downloading data...", expanded=True) as status:
-            st.write("Mi collego al sito dell'ECHA")
-            url = 'https://chem.echa.europa.eu/'
-            # Inizializzo tutto l'ambaradam per scrappare sto dannato sito dell'ECHA
-            # Parte del codice è scopiazzato da un eroe che è riuscito a far funzionare Selenium su Streamlit. 
-            driver = None
-            try:
-                # Using on Local
-                st.write("Inizializzo i processi di Selenium")
-                options = webdriver.ChromeOptions()
-                options.add_argument('--headless')
-                #options.add_argument('--disable-gpu')
-                #options.add_argument('--window-size=1920,1200')
-                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                        options=options)
-                # st.write(f"DEBUG:DRIVER:{driver}")
-                driver.get(url)
-                time.sleep(2)
-                st.write("Cerco la checkbox dei termini e condizioni")
-                driver.get("https://chem.echa.europa.eu/")
-                driver.set_window_size(1920, 1080)
-                driver.find_element(By.XPATH, "//iucdas-mod-substance-search-app").click()
-                #driver.find_element(By.CSS_SELECTOR, "iucdas-mod-substance-search-app").click()
-                time.sleep(2)
-                #st.write("Checkbox cliccata. Ora cerco l'elemento selezionato. ")
-                #search_box = driver.find_element(by=By.CLASS_NAME, value='das-root-container')
-                #html_doc = driver.page_source
+            substance = echa_substance_select
+            final_url = search_dossier(substance)
+            if final_url:
+                st.page_link(label='Riassunto tossicologico completo sul sito ECHA', page=final_url)
+                summary_content = rq.get(final_url).text
+                echa_pandas(summary_content)
 
-                ##### put the magic here 
 
-                #html_doc
-                driver.quit()
-                #soup = BeautifulSoup(html_doc, "html.parser")
 
-            except Exception as e:
-                st.write(f"{e}")
-            finally:
-                if driver is not None: driver.quit()
 
 
 elif source ==':blue[CIR]':
